870ce657a55a69a8cddb87f9ca4cc884db458682
Elijah Erickson <mnelijahre@gmail.com>
2024-08-22 12:54:17 -0500

    made it dope as hell

--- meetTheOfficers/script.js
+++ meetTheOfficers/script.js
@@ -3,23 +3,27 @@ const person2 = document.querySelector("#person2");
 const person3 = document.querySelector("#person3");
 const person4 = document.querySelector("#person4");
 const people = document.querySelectorAll(".people");
-
+const backgroundMen = document.querySelector("#backgroundMen");
 
 const personName = document.querySelector("#personName");
 const personMajor = document.querySelector("#personMajor");
 const personDetails = document.querySelector("#personDetails");
 
-function person(name, major, details){
+function person(name, major, details, backgroundSelected){
     this.name = name;
     this.major = major;
     this.details = details;
+    this.backgroundSelected = backgroundSelected;
 }
 
-const personOneDetails = person("Sam", "Computer Science", "N/A");
-const personTwoDetails = person("John", "Computer Science", "N/A");
-const personThreeDetails = person("Bret", "Computer Science", "N/A");
-const personFourDetails = person("George", "Computer Science", "N/A");
+const personOneDetails = new person("Sam", "Computer Science", "A good person", "res/person1.jpg");
+const personTwoDetails = new person("John", "Biology", "Nobody special", "res/person2.jpg");
+const personThreeDetails = new person("Bret", "Art", "A special man", "res/person3.jpg");
+const personFourDetails = new person("George", "Theater", "A dork", "res/person4.jpg");
 
+updateDisplay("person1")
+
+console.log(personOneDetails.name);
 
 console.log(people);
 people.forEach((p)=>{
@@ -31,26 +35,26 @@ people.forEach((p)=>{
 
 function updateDisplay(pID){
     const theme = pID + "-page";
-    let name, major, details;
+    let personSelected;
 
     if (pID == "person1"){
-        name  = personOneDetails.name;
-        major = personOneDetails.major;
-        details = personOneDetails.details;
+        personSelected = personOneDetails;
     }
     if (pID == "person2"){
-        name = personTwoDetails.name;
-        major = personTwoDetails.major;
-        details = personTwoDetails.details;
+        personSelected = personTwoDetails;
     }
     if (pID == "person3"){
-        name = personThreeDetails.name;
-        major = personThreeDetails.major;
-        details = personThreeDetails.details;
+        personSelected = personThreeDetails;
     }
     if (pID == "person4"){
-        
+        personSelected = personFourDetails;
     }
+
+    personName.innerHTML = personSelected.name;
+    personMajor.innerHTML = personSelected.major;
+    personDetails.innerHTML = personSelected.details;
+    backgroundMen.style.backgroundImage = "url(" + personSelected.backgroundSelected + ")";
+    //personSelected.backgroundSelected;
 
     document.querySelectorAll(".people").forEach((p)=>{
         p.classList.remove("selectedPerson");
@@ -58,6 +62,5 @@ function updateDisplay(pID){
     document.querySelector("#" + pID).classList.add("selectedPerson");
 
     
-    console.log(theme);
 }
 
